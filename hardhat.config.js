ce8fa502ac75c365977d54c9fe21672b6fe9a1e0
Ess <127221085+EstherLavender@users.noreply.github.com>
2024-11-17 06:34:35 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,3 +4,29 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.27",
 };
+{
+  "contracts": {
+    "Calculator": {
+      "source": "./lock.sol",
+      "aliases": {
+        "testing": "0x0000000000000006"
+      }
+    }
+  },
+  "networks": {...},
+  "accounts": {...},
+  "deployments": {...}
+}
+{
+  "contracts": {
+    "Calculator": {
+      "source": "./usermanager.sol",
+      "aliases": {
+        "testing": "0x0000000000000005"
+      }
+    }
+  },
+  "networks": {...},
+  "accounts": {...},
+  "deployments": {...}
+}
